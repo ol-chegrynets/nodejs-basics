813fe8ac831094cdd81337e3245f5695aecc2da8
shura73 <ol.chegrynets@gmail.com>
2025-01-30 18:58:51 +0100

    lesson 1

---
+++ src/controllers/students.js
@@ -0,0 +1,27 @@
+import createHttpError from 'http-errors';
+import { getAllStudents, getStudentById } from '../services/students.js';
+
+export const getStudentsController = async (req, res, next) => {
+  const students = await getAllStudents();
+
+  res.json({
+    status: 200,
+    message: 'Successfully found students!',
+    data: students,
+  });
+};
+
+export const getStudentByIdController = async (req, res) => {
+  const { studentId } = req.params;
+  const student = await getStudentById(studentId);
+
+  if (!student) {
+    throw createHttpError(404, 'Student not found');
+  }
+  // Відповідь, якщо контакт знайдено
+  res.json({
+    status: 200,
+    message: `Successfully found student with id ${studentId}!`,
+    data: student,
+  });
+};
