3cbfc8b5e3002e6081b4a5db074832827f30497b
Oleksandr <ol.chegrynets@gmail.com>
2025-02-01 11:38:59 +0100

    Modul 3 (#1)
    * lesson 1
    
    * lesson 1
    
    * end commit

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { ENV_VARS } from './constants/env.js';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllStudents, getStudentById } from './services/students.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandlerMiddlewqares } from './middlewares/errorHandler.js';
+import router from './routers/students.js';
 
 const PORT = Number(getEnvVar(ENV_VARS.PORT, 3000));
 
@@ -26,52 +28,17 @@ export const startServer = () => {
     console.log('====================================');
     next();
   });
-
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello, Home Page!',
     });
   });
-  
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
+  app.use(router);
 
-    res.status(200).json({
-      data: students,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/students/:studentId', async (req, res) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-
-    // Відповідь, якщо контакт не знайдено
-    if (!student) {
-      res.status(404).json({
-        message: 'Student not found',
-      });
-      return;
-    }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      data: student,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandlerMiddlewqares);
 
   app.listen(PORT, () => {
     console.log('====================================');
