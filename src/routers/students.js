ea6e79cbf10fad3fe4b40df5bddc960ff3f74b70
shura73 <ol.chegrynets@gmail.com>
2025-02-04 18:31:39 +0100

    temp

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import {
   createStudentController,
@@ -9,6 +8,12 @@ import {
   upsertStudentController,
 } from '../controllers/students.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createStudentSchema,
+  updateStudentSchema,
+} from '../validation/students.js';
+import { isValidId } from '../middlewares/imValidId.js';
 
 const router = express.Router();
 const jsonParser = express.json({
@@ -16,27 +21,42 @@ const jsonParser = express.json({
   limit: '100kb',
 });
 
-
 router.get('/students', ctrlWrapper(getStudentsController));
 
-router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
+router.get(
+  '/students/:studentId',
+  isValidId,
+  ctrlWrapper(getStudentByIdController),
+);
 
+router.post(
+  '/students',
+  jsonParser,
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
 
-router.post('/students', jsonParser, ctrlWrapper(createStudentController));
-
-router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
+router.delete(
+  '/students/:studentId',
+  isValidId,
+  validateBody(createStudentSchema),
+  ctrlWrapper(deleteStudentController),
+);
 
 router.put(
   '/students/:studentId',
   jsonParser,
+  isValidId,
+  validateBody(createStudentSchema),
   ctrlWrapper(upsertStudentController),
 );
 
 router.patch(
   '/students/:studentId',
   jsonParser,
+  isValidId,
+  validateBody(updateStudentSchema),
   ctrlWrapper(patchStudentController),
 );
-
 
 export default router;
